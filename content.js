d302cabd339aef8d9f3ed8abd564ebbc60a6106e
ArchITech <swtdavid@gmail.com>
2016-12-15 00:32:31 -0800

    Try div insert chatbox without iframe

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
 // Listen for messages
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
     if (msg.text === 'report_back') {
@@ -9,26 +10,48 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
         responseObj.link = document.location.href;
         console.log(responseObj);
         sendResponse(responseObj);
+    
+
+        chatboxDiv  = document.createElement ("div");
+        indexURL = chrome.extension.getURL ("chatbox/index.html");
+
+        chatboxDiv.innerHTML = '<object type="text/html" id="socket-chatbox-embeded" data="'+indexURL+'" ></object>';
+
+        document.body.insertBefore(chatboxDiv, document.body.firstChild);
     }
 });
 
-// var socket = io('http://localhost:4321');
-
-var chatboxIFrame  = document.createElement ("iframe");
-chatboxIFrame.src  = chrome.extension.getURL ("chatbox/index.html");
-chatboxIFrame.id="chatbox-iframe";
 
 
-    // var iFrame = document.getElementById( 'iFrame1' );
+// setTimeout(function(){ 
+//             console.log('now');
+//             console.log(window.location.href);
+
+//             document.removeChild(window.chatboxIFrame);
+
+//             window.chatboxIFrame.width  = window.chatboxIFrame.contentWindow.document.body.scrollWidth;
+//             window.chatboxIFrame.height = window.chatboxIFrame.contentWindow.document.body.scrollHeight;
+// }, 12000);
 
 
-document.body.insertBefore(chatboxIFrame, document.body.firstChild);
+
+window.addEventListener("message", resizeIFrameToFitContent, false);
+
+
+
 
 
 function resizeIFrameToFitContent() {
 
-    chatboxIFrame.width  = chatboxIFrame.contentWindow.document.body.scrollWidth;
-    chatboxIFrame.height = chatboxIFrame.contentWindow.document.body.scrollHeight;
+    console.log('resizing');
+
+    // setTimeout(function(){ 
+
+            chatboxDiv.width  = chatboxDiv.contentWindow.document.body.scrollWidth;
+    //         chatboxIFrame.height = chatboxIFrame.contentWindow.document.body.scrollHeight;
+     
+    // }, 3000);
+
 }
 
-resizeIFrameToFitContent();
+// resizeIFrameToFitContent();
